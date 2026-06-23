f83c5ca2016034325d621509d520aec20a4a9f92
zahidhasansohag <sohag.sag@gmail.com>
2026-06-24 00:45:58 +0600

    Update DashboardCard.jsx

--- components/DashboardCard.jsx
+++ components/DashboardCard.jsx
@@ -1,14 +1,11 @@
-export default function DashboardCard({
-  title,
-  value,
-}) {
+export default function DashboardCard({ title, value }) {
   return (
-    <div className="bg-white rounded-2xl shadow p-6">
-      <h2 className="text-gray-500">
+    <div className="bg-white rounded-2xl shadow-lg p-6">
+      <h2 className="text-gray-500 text-sm">
         {title}
       </h2>
 
-      <p className="text-3xl font-bold mt-2">
+      <p className="text-3xl font-bold mt-3">
         {value}
       </p>
     </div>
